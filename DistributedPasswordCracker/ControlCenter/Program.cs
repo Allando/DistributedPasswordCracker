cd45d3955a77d74ff081f53e2a3d68aed1eed1ac
Unknown <alla2634@edu.easj.dk>
2017-07-22 16:13:08 +0200

    Client-server code is ready to be modified

--- DistributedPasswordCracker/ControlCenter/Program.cs
+++ DistributedPasswordCracker/ControlCenter/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
 
 namespace ControlCenter
 {
@@ -10,6 +13,33 @@ namespace ControlCenter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CC");
+
+            int number = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); //
+            UdpClient udpClient = new UdpClient("127.0.0.1", 9999);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9999); //
+            //udpClient.Connect(RemoteIpEndPoint); //
+
+            Console.Write("State name: ");
+            String name = Console.ReadLine();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(name + " " + number + " hello");
+
+                udpClient.Send(sendBytes, sendBytes.Length); //, RemoteEndPoint);
+                Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                //Client is now activated");
+
+                string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                Console.WriteLine(receivedData);
+                number++;
+
+                Thread.Sleep(100);
+            }
         }
     }
 }
