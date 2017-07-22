342202afa790285cdacc24e615ac9a66e403f5f5
Unknown <alla2634@edu.easj.dk>
2017-07-22 14:15:37 +0200

    Added Class in DistributedPasswordCracker nad New project to solution

--- DistributedPasswordCracker/DistributedPasswordCracker/Connection/UdpClient.cs
+++ DistributedPasswordCracker/DistributedPasswordCracker/Connection/UdpClient.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistributedPasswordCracker
+namespace DistributedPasswordCracker.Connection
 {
-    class Program
+    class UdpClient
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
--- DistributedPasswordCracker/ZombieCracker/Program.cs
+++ DistributedPasswordCracker/ZombieCracker/Program.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DistributedPasswordCracker
+namespace ZombieCracker
 {
     class Program
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
