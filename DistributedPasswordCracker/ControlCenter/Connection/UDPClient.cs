3e63f27a98e985ff3af89663adee9b90bdff34ee
Unknown <alla2634@edu.easj.dk>
2017-07-22 14:42:05 +0200

    Emergency commit and push
    Visual Studio 2017 with Resharper does not accept basic C# syntax

--- DistributedPasswordCracker/ControlCenter/Connection/UDPClient.cs
+++ DistributedPasswordCracker/ControlCenter/Connection/UDPClient.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ControlCenter.Connection
 {
-    class UDPClient
+    class UdpClient
     {
+        UdpClient udpClient = new UdpClient();
+
+        
+
     }
 }
--- DistributedPasswordCracker/ZombieCracker/Connection/UDPServer.cs
+++ DistributedPasswordCracker/ZombieCracker/Connection/UDPServer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ZombieCracker.Connection
 {
-    class UDPServer
+    class UdpServer
     {
     }
 }
